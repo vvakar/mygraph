ce17d636aa2fae499443c60379b8a0eebb334068
Val Vakar <vvakar@conductor.com>
2014-07-28 06:57:20 -0400

    Added BFS/DFS

--- src/main/java/vvakar/graph/traversal/UndirectedBreadthFirstIterator.java
+++ src/main/java/vvakar/graph/traversal/UndirectedBreadthFirstIterator.java
@@ -1,59 +1,34 @@
 package vvakar.graph.traversal;
-import vvakar.graph.structure.Edge;
-import vvakar.graph.structure.Graph;
-import vvakar.graph.structure.Vertex;
+import vvakar.graph.interfaces.Edge;
+import vvakar.graph.interfaces.Graph;
+import vvakar.graph.interfaces.Vertex;
 
-import javax.annotation.Nullable;
 import java.util.ArrayDeque;
 import java.util.Collection;
 import java.util.Deque;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
 
 /**
  * Iterate breadth-first over the supplied graph.
  * @author vvakar
  *         Date: 7/27/14
  */
-public class UndirectedBreadthFirstIterator implements Iterator<Vertex> {
-    private final Graph<Vertex,Edge> graph;
-    private final Set<Vertex> seen = new HashSet<Vertex>();
-    private Vertex currentVertex;
-    private Deque<Vertex> queue = new ArrayDeque<Vertex>();
+public class UndirectedBreadthFirstIterator<V extends Vertex, E extends Edge> extends AbstractUndirectedGraphIterator<V,E> {
+    private Deque<V> queue = new ArrayDeque<V>();
 
-    /**
-     * Traverse graph starting with arbitrarily chosen vertex.
-     * @param graph
-     */
-    public UndirectedBreadthFirstIterator(Graph graph) {
-        this(graph, graph.getVertices().isEmpty() ? null : (Vertex) graph.getVertices().iterator().next());
+    public UndirectedBreadthFirstIterator(Graph<V,E> graph) {
+        super(graph);
     }
 
-    /**
-     * Traverse graph starting at vertex indicated by <code>startingPoint</code>
-     * @param graph
-     * @param startingPoint
-     */
-    public UndirectedBreadthFirstIterator(Graph graph, @Nullable Vertex startingPoint) {
-        this.graph = graph;
-        this.currentVertex = startingPoint;
-
-        if(startingPoint != null)
-            seen.add(startingPoint);
+    public UndirectedBreadthFirstIterator(Graph<V,E> graph, V start) {
+        super(graph, start);
     }
 
     @Override
-    public boolean hasNext() {
-        return currentVertex != null;
-    }
-
-    @Override
-    public Vertex next() {
-        Vertex previous = currentVertex;
+    public V next() {
+        V previous = currentVertex;
         if(currentVertex != null) {
-            Collection<Vertex> ns = graph.getNeighborsOf(currentVertex);
-            for(Vertex v : ns) {
+            Collection<V> ns = graph.getNeighborsOf(currentVertex);
+            for(V v : ns) {
                 if(!seen.contains(v)) {
                     queue.addLast(v);
                     seen.add(v);
@@ -63,10 +38,5 @@ public class UndirectedBreadthFirstIterator implements Iterator<Vertex> {
             currentVertex = queue.isEmpty() ? null : queue.removeFirst();
         }
         return previous;
-    }
-
-    @Override
-    public void remove() {
-        throw new UnsupportedOperationException();
     }
 }
--- src/test/java/vvakar/graph/traversal/UndirectedBreadthFirstIteratorTest.java
+++ src/test/java/vvakar/graph/traversal/UndirectedBreadthFirstIteratorTest.java
@@ -2,47 +2,27 @@ package vvakar.graph.traversal;
 
 import org.junit.Before;
 import org.junit.Test;
-import vvakar.graph.structure.Edge;
-import vvakar.graph.structure.Graph;
-import vvakar.graph.structure.SimpleGraph;
-import vvakar.graph.structure.Vertex;
-import static vvakar.graph.structure.GraphFactory.*;
-
+import vvakar.graph.interfaces.Vertex;
 import static org.junit.Assert.*;
 
 /**
  * @author vvakar
  *         Date: 7/27/14
  */
-public class UndirectedBreadthFirstIteratorTest {
-
-    Graph graph;
-
-    /*
-                 v1 --- v2 --- v4
-                   \
-                    \__ v3
-     */
-    Vertex v1 = vertex("v1"), v2 = vertex("v2"), v3 = vertex("v3"), v4 = vertex("v4");
-    Edge e12 = undirectedEdge(v1, v2), e13 = undirectedEdge(v1, v3), e24 = undirectedEdge(v2, v4);
-
+public class UndirectedBreadthFirstIteratorTest extends AbstractUndirectedIteratorTest {
     @Before
     public void before() {
-        graph = new SimpleGraph();
-
+        super.before();
     }
 
     @Test
     public void testEmpty() {
-        assertFalse(new UndirectedBreadthFirstIterator(graph).hasNext());
+        assertFalse(new UndirectedBreadthFirstIterator(emptyGraph).hasNext());
     }
 
     @Test
     public void testNonEmpty() {
-        graph.put(e12);
-        graph.put(e13);
-        graph.put(e24);
-        UndirectedBreadthFirstIterator iterator = new UndirectedBreadthFirstIterator(graph, v1);
+        UndirectedBreadthFirstIterator iterator = new UndirectedBreadthFirstIterator(nonEmptyGraph, v1);
         assertTrue(iterator.hasNext());
         assertEquals(v1, iterator.next());
         Vertex v2orv3 = iterator.next();
