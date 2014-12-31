c41e14d7aa33915a6bb4bbaa66ba4de501875ccd
Val Vakar <vvakar@conductor.com>
2014-12-31 20:56:34 +0000

    updates

--- src/main/java/vvakar/strings/SuffixArrays.java
+++ src/main/java/vvakar/strings/SuffixArrays.java
@@ -2,6 +2,7 @@ package vvakar.strings;
 
 import javax.xml.stream.events.Characters;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.ObjectStreamField;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
@@ -9,9 +10,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Formatter;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -22,27 +26,62 @@ import java.util.regex.Pattern;
 public class SuffixArrays {
 
     public static void main(String[]asdf) throws Exception {
-        findLargestRepeatedSubstring("/Users/vvakar/Downloads/mobydick.txt");
+//        findLargestRepeatedSubstring("/Users/vvakar/Downloads/mobydick.txt");
+//        findLargestRepeatedSubstring("/Users/vvakar/Downloads/pride.txt");
+//        findLargestRepeatedSubstring("/Users/vvakar/Downloads/janeeyre.txt");
+//
+
+        int N = 30;
+        countTopSubstringsOfSize("/Users/vvakar/Downloads/mobydick.txt", N);
+        countTopSubstringsOfSize("/Users/vvakar/Downloads/pride.txt", N);
+        countTopSubstringsOfSize("/Users/vvakar/Downloads/janeeyre.txt", N);
     }
 
-    public static void findLargestRepeatedSubstring(String filename) throws Exception {
-        final FileInputStream in = new FileInputStream(filename);
-        int c;
-        boolean prevIsLetter = false;
-        final StringBuilder sb = new StringBuilder();
-        while((c = in.read()) != -1) {
-            boolean isLetter = Character.isLetter(c);
+    public static void countTopSubstringsOfSize(String filename, int size) throws Exception {
+        final MyString wholeBook = readFully(filename);
 
-            if(isLetter) {
-                sb.append((char)c);
-            } else if(prevIsLetter) {
-                sb.append(' ');
-            }
+        MyString[] suffixes = buildSuffixArray(wholeBook, size);
 
-            prevIsLetter = isLetter;
+        Map<MyString, Integer> counts = new HashMap<MyString, Integer>();
+        for(int i = 0; i < suffixes.length -1; ++i) {
+            int val = counts.getOrDefault(suffixes[i], 0);
+            counts.put(suffixes[i], val + 1);
         }
 
-        final MyString wholeBook = new MyString(sb.toString().toCharArray());
+
+        // top N - get a min heap
+        // when heap size > N, remove one item
+        // will be left with the max N items
+        int N = 10;
+        PriorityQueue<MyStringCount> pq = new PriorityQueue<MyStringCount>();
+        for(Map.Entry<MyString, Integer> entry : counts.entrySet()) {
+            pq.add(new MyStringCount(entry.getKey(), entry.getValue()));
+            if(pq.size() > N) pq.poll();
+        }
+
+        System.out.println("\n\nTOP COUNTS for " + filename);
+        while(!pq.isEmpty()) {
+            MyStringCount c = pq.poll();
+            System.out.println("--> " + c.myString + " = " + c.count);
+        }
+
+    }
+
+    private static final class MyStringCount implements Comparable<MyStringCount> {
+        public final int count;
+        public final MyString myString;
+        MyStringCount(MyString myString, int count) { this.myString = myString; this.count = count; }
+
+        @Override
+        public int compareTo(MyStringCount o) {
+            if(o == null) return 1;
+            return Integer.compare(count, o.count);
+        }
+    }
+
+
+    public static void findLargestRepeatedSubstring(String filename) throws Exception {
+        final MyString wholeBook = readFully(filename);
 
 
         List<MyString> suffixes = new LinkedList<MyString>();
@@ -52,8 +91,7 @@ public class SuffixArrays {
             if(Character.isLetter(str.charAt(0))) suffixes.add(str);
         }
 
-        MyString[] sorted = suffixes.toArray(new MyString[0]);
-        Arrays.sort(sorted);
+        MyString[] sorted = buildSuffixArray(wholeBook, Integer.MAX_VALUE);
         int maxOverlap = -1;
         MyString maxOverlapString = null;
 
@@ -70,8 +108,21 @@ public class SuffixArrays {
 
 
 
-        System.out.println("Max overlapping string: \n\n" + (maxOverlapString == null ? "---" :
+        System.out.println("Max overlapping string in " + filename + ": \n\n" + (maxOverlapString == null ? "---" :
                 maxOverlapString.substring(0, maxOverlap)));
+    }
+
+    private static MyString[] buildSuffixArray(MyString master, int maxLen) {
+        List<MyString> suffixes = new LinkedList<MyString>();
+        final int length = master.length();
+        for(int i = 0; i < length; ++i) {
+            MyString str = master.substring(i, Math.min(i + maxLen, master.length()));
+            if(Character.isLetter(str.charAt(0))) suffixes.add(str);
+        }
+
+        MyString[] sorted = suffixes.toArray(new MyString[0]);
+        Arrays.sort(sorted);
+        return sorted;
     }
 
     /**
@@ -80,6 +131,7 @@ public class SuffixArrays {
     private static final class MyString implements Comparable<MyString> {
         private final char[] arr;
         private final int from, to;
+        private Integer hashcode;
 
         MyString(char[] arr) {
             this(arr, 0, arr.length);
@@ -134,5 +186,43 @@ public class SuffixArrays {
             return new String(Arrays.copyOfRange(arr, from, to));
         }
 
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            MyString myString = (MyString) o;
+            return this.compareTo(myString) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            if(hashcode == null) {
+                hashcode = this.toString().hashCode();
+            }
+            return hashcode;
+        }
     }
+
+
+    private static MyString readFully(String filename) throws IOException {
+        final FileInputStream in = new FileInputStream(filename);
+        int c;
+        boolean prevIsReadable = false;
+        final StringBuilder sb = new StringBuilder();
+        while((c = in.read()) != -1) {
+            boolean isLetter = Character.isLetter(c);
+
+            if(isLetter || '.' == c || '!' == c || '?' == c) {
+                sb.append(Character.toLowerCase((char)c));
+                prevIsReadable = true;
+            } else if(prevIsReadable) {
+                sb.append(' ');
+            }
+        }
+        in.close();
+
+        return new MyString(sb.toString().toCharArray());
+    }
+
 }
