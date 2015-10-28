8317e01e77ef28229fe2f46a0582c87d8dcf9749
valentin.vakar <valentin.vakar@virtualinstruments.com>
2015-10-28 10:53:28 +0000

    Basic Karger Graph computation

---
+++ src/main/java/vvakar/algos1/KargerGraph.java
@@ -0,0 +1,170 @@
+package vvakar.algos1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Created by valentin.vakar on 10/27/15.
+ */
+public class KargerGraph {
+
+    public static void main(String[] asdf) throws Exception {
+        KargerGraph graph = new KargerGraph();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(KargerGraph.class.getClassLoader().getResourceAsStream("KargerMinCut.txt")));
+        while (reader.ready()) {
+            String[] line = reader.readLine().split("\\s+");
+            String v = line[0];
+            for (int i = 1; i < line.length; ++i) {
+                graph.recordEdge(v, line[i]);
+            }
+        }
+
+        System.out.println("HELLOOO " + graph.minCut());
+    }
+
+    public Map<String, Set<Edge>> verticesToEdges = new HashMap<>();
+    private Set<Edge> allEdges = new HashSet<>();
+
+    public void recordEdge(String a, String b) {
+        if (a.equals(b)) return; // ignore self-edges
+
+        Set<Edge> edgesA = verticesToEdges.getOrDefault(a, new HashSet<>());
+        Set<Edge> edgesB = verticesToEdges.getOrDefault(b, new HashSet<>());
+
+        int i = 0;
+        Edge newEdge = new Edge(a, b, i);
+        while (allEdges.contains(newEdge)) {
+            newEdge = new Edge(a, b, ++i);
+        }
+        edgesA.add(newEdge);
+        edgesB.add(newEdge);
+        allEdges.add(newEdge);
+        verticesToEdges.put(a, edgesA);
+        verticesToEdges.put(b, edgesB);
+    }
+
+    /**
+     * Contract two vertices and return the new key
+     */
+    public String contractVertices(String a, String b) {
+        if (!verticesToEdges.containsKey(a)) throw new IllegalStateException("Cant find key " + a);
+        if (!verticesToEdges.containsKey(b)) throw new IllegalStateException("Cant find key " + b);
+        String key = mergeKeys(a, b);
+        if (verticesToEdges.containsKey(key)) throw new IllegalStateException("Key already there: " + key);
+
+        Set<Edge> aEdges = verticesToEdges.get(a);
+        Set<Edge> bEdges = verticesToEdges.get(b);
+        Set<Edge> abEdges = new HashSet<>(aEdges);
+        abEdges.addAll(bEdges);
+
+
+        for (Edge edge : abEdges) {
+            allEdges.remove(edge);
+            if ((edge.a.equals(a) && edge.b.equals(b) || edge.b.equals(a) && edge.a.equals(b))) {
+                // swallow
+            } else if (edge.a.equals(a) || edge.a.equals(b)) {
+                verticesToEdges.get(edge.b).remove(edge);
+                recordEdge(key, edge.b);
+            } else {
+                verticesToEdges.get(edge.a).remove(edge);
+                recordEdge(edge.a, key);
+            }
+        }
+
+        verticesToEdges.remove(a);
+        verticesToEdges.remove(b);
+        return key;
+    }
+
+    /**
+     * Utilities
+     */
+    public int minCut() {
+        int min = Integer.MAX_VALUE;
+
+        Map<String, Set<Edge>> verticesToEdgesOriginal = clone(verticesToEdges);
+
+        for (int i = 0; i < verticesToEdgesOriginal.size() * 100; ++i) {
+            List<String> keys = new ArrayList<>(verticesToEdges.keySet());
+            while (keys.size() > 2) {
+                Collections.shuffle(keys);
+                String a = keys.remove(keys.size() - 1);
+                String b = keys.remove(keys.size() - 1);
+
+                String newKey = contractVertices(a, b);
+                keys.add(newKey);
+            }
+
+            min = Math.min(min, allEdges.size());
+            verticesToEdges = clone(verticesToEdgesOriginal);
+            allEdges = verticesToEdges.values().stream().flatMap(s -> s.stream()).collect(Collectors.toSet());
+        }
+
+        return min;
+    }
+
+    private Set<Edge> clone(Set<Edge> edges) {
+        Set<Edge> cl = new HashSet<>(edges.size());
+        for (Edge e : edges) {
+            cl.add(new Edge(e.a, e.b, e.i));
+        }
+        return cl;
+    }
+
+    private Map<String, Set<Edge>> clone(Map<String, Set<Edge>> map) {
+        Map<String, Set<Edge>> cl = new HashMap<>(map.size());
+        for (Map.Entry<String, Set<Edge>> entry : map.entrySet()) {
+            cl.put(entry.getKey(), clone(entry.getValue()));
+        }
+
+        return cl;
+    }
+
+    public static final class Edge {
+        public String a, b;
+        public int i;
+
+        public Edge(String a, String b, int i) {
+            this.a = a;
+            this.b = b;
+            this.i = i;
+        }
+
+        @Override
+        public String toString() {
+            return a + "<->" + b + ":" + i;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Edge edge = (Edge) o;
+
+            if (i != edge.i) return false;
+            return (a.equals(edge.a) && b.equals(edge.b)) || ((b.equals(edge.a) && a.equals(edge.b)));
+        }
+
+        @Override
+        public int hashCode() {
+            int result = 31 * (a.hashCode() + b.hashCode());
+            result = 31 * result + i;
+            return result;
+        }
+
+    }
+
+    public static String mergeKeys(String a, String b) {
+        return "[" + a + "][" + b + "]";
+    }
+
+}
---
+++ src/test/java/vvakar/algos1/KargerMinCutTest.java
@@ -0,0 +1,108 @@
+package vvakar.algos1;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static vvakar.algos1.KargerGraph.*;
+
+public class KargerMinCutTest {
+    private KargerGraph graph;
+
+    @Before
+    public void before() {
+        graph = new KargerGraph();
+
+    }
+
+    @Test
+    public void testSupportsDuplicateEdges() {
+        graph.recordEdge("x", "y");
+        graph.recordEdge("x", "y");
+        graph.recordEdge("x", "y");
+
+        assertEquals(3, graph.verticesToEdges.get("x").size());
+        assertEquals(3, graph.verticesToEdges.get("y").size());
+    }
+
+    @Test
+    public void testSupportsContraction() {
+        graph.recordEdge("a", "b");
+        graph.recordEdge("a", "c");
+        graph.recordEdge("a", "d");
+        graph.recordEdge("c", "d");
+
+        int expectedEdgeCount = 0;
+
+        for (Edge e : graph.verticesToEdges.get("a")) {
+            if (!(e.a.equals("a") && e.b.equals("b")) || (e.b.equals("a") && e.a.equals("b"))) {
+                ++expectedEdgeCount;
+            }
+        }
+        for (Edge e : graph.verticesToEdges.get("b")) {
+            if (!(e.a.equals("a") && e.b.equals("b")) || (e.b.equals("a") && e.a.equals("b"))) {
+                ++expectedEdgeCount;
+            }
+        }
+
+        String newKey = graph.contractVertices("a", "b");
+
+        assertFalse(graph.verticesToEdges.containsKey("a"));
+        assertFalse(graph.verticesToEdges.containsKey("b"));
+        assertEquals(expectedEdgeCount, graph.verticesToEdges.get(newKey).size());
+    }
+
+    @Test
+    public void test() {
+        graph.recordEdge("a", "b");
+        graph.recordEdge("a", "c");
+        graph.recordEdge("a", "d");
+        graph.recordEdge("c", "d");
+        assertEquals(1, graph.minCut());
+    }
+
+    @Test
+    public void test2() {
+        graph.recordEdge("a", "b");
+        graph.recordEdge("a", "c");
+        graph.recordEdge("a", "d");
+        graph.recordEdge("b", "c");
+        graph.recordEdge("b", "d");
+        graph.recordEdge("c", "d");
+
+        graph.recordEdge("e", "f");
+        graph.recordEdge("e", "g");
+        graph.recordEdge("e", "h");
+        graph.recordEdge("f", "g");
+        graph.recordEdge("f", "h");
+        graph.recordEdge("g", "h");
+
+        graph.recordEdge("b", "e");
+        graph.recordEdge("d", "g");
+
+        assertEquals(2, graph.minCut());
+    }
+
+    @Test
+    public void test3() {
+        graph.recordEdge("a", "b");
+        graph.recordEdge("a", "c");
+        graph.recordEdge("a", "d");
+        graph.recordEdge("b", "c");
+        graph.recordEdge("b", "d");
+        graph.recordEdge("c", "d");
+
+        graph.recordEdge("e", "f");
+        graph.recordEdge("e", "g");
+        graph.recordEdge("e", "h");
+        graph.recordEdge("f", "g");
+        graph.recordEdge("f", "h");
+        graph.recordEdge("g", "h");
+
+        graph.recordEdge("b", "e");
+        graph.recordEdge("e", "b");// dupe
+        graph.recordEdge("d", "g");
+
+        assertEquals(3, graph.minCut());
+    }
+}
